9d96a9154103029cce243febd4c6a8e57fe07a4f
cyacedev <github@cyace.dev>
2020-09-04 11:13:51 +0200

    added barebone project

---
+++ Runner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using conway.lib;
+
+namespace conway
+{
+    class Runner
+    {
+        private static int PlayfieldSize = new Int32();
+        static void Main(string[] args)
+        {
+            do
+            {
+              Console.WriteLine("Field size? "); 
+              PlayfieldSize = int.Parse(Console.ReadLine());
+            } while (PlayfieldSize == 0 || PlayfieldSize > 50 );
+            ConwayGame conway = new ConwayGame(PlayfieldSize);
+        }
+    }
+}
---
+++ src/ConwayGame.cs
@@ -0,0 +1,17 @@
+namespace conway.lib
+{
+    public class ConwayGame
+    {
+        private readonly int PlayfieldSize;
+
+        public ConwayGame(int PlayfieldSize)
+        {
+            this.PlayfieldSize = PlayfieldSize;
+        }
+
+        public void Run()
+        {
+            
+        }
+    }
+}
