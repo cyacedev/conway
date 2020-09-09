d49649b9cc954a125843953d0c1553e3a876211d
cyacedev <github@cyace.dev>
2020-09-09 09:01:21 +0200

    🐛 ✨ basic iteration works

--- src/ConwayGame.cs
+++ src/ConwayGame.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 
 namespace conway.lib
 {
@@ -16,77 +16,99 @@ namespace conway.lib
 
         public void Run()
         {
-            GenerateDefinedAmountOfCells();
+            GenerateCellsWithProbability(50);
+            Console.WriteLine($"Generated Cells: {cells.Count}");
             for (int i = 0; i < 1000; i++)
             {
                 IterateSimulation();
             }
+            Console.WriteLine($"Alive after 1000 iterations: {cells.Count}");
         }
 
-        private void GenerateDefinedAmountOfCells()
+        private void GenerateDefinedAmountOfCells(int AmountOfCells)
         {
-            for (int i = 0; i < StarterSize / 2; i++)
+            for (int i = 0; i < AmountOfCells; i++)
             {
                 bool added = false;
                 do
                 {
                     var Random = new System.Random();
-                    var x = Random.Next(StarterSize * -1, StarterSize);
-                    var y = Random.Next(StarterSize * -1, StarterSize);
+                    var x = Random.Next(StarterSize * -1, StarterSize + 1);
+                    var y = Random.Next(StarterSize * -1, StarterSize + 1);
                     added = cells.TryAdd(new CellCoords(x, y), new Cell());
                 } while (added == false);
             }
         }
 
-        private void IterateSimulation()
+        private void GenerateCellsWithProbability(int PromilleMax)
         {
-            int Neighbours = 0;
-            Cell Dummy;
-            bool Exists;
-            for (int x = StarterSize * -1; x < StarterSize; x++)
+            for (int x = StarterSize * -1; x <= StarterSize; x++)
             {
-                for (int y = StarterSize * -1; y < StarterSize; y++)
+                for (int y = StarterSize * -1; y <= StarterSize; y++)
                 {
-                    Neighbours = GetAliveNeighbours(x, y);
-                    switch (Neighbours)
+                    var Random = new System.Random();
+                    if (Random.Next(1001) <= PromilleMax)
                     {
-                        case 2:
-                            //TODO: copy over to new List
-                            break;
-                        case 3:
-                            Exists = cells.TryGetValue(new CellCoords(x, y), out Dummy);
-                            if (Exists == false)
-                            {
-                                //TODO: add alive Cell to new List if dead and heat+
-                            }
-                            else
-                            {
-
-                                //TODO: copy over to new List
-                            }
-                            break;
-                        default:
-                            Exists = cells.TryGetValue(new CellCoords(x, y), out Dummy);
-                            if (Exists)
-                            {
-                                //TODO: kill cell to new List if alive and heat-
-                            }
-                            else
-                            {
-                                //TODO: copy over to new List
-                            }
-                            break;
+                        cells.Add(new CellCoords(x, y), new Cell());
                     }
                 }
             }
         }
 
+        private void IterateSimulation()
+        {
+            var newCells = new Dictionary<CellCoords, Cell>(new CellCoordsComparer());
+            foreach (System.Collections.Generic.KeyValuePair<conway.lib.CellCoords, conway.lib.Cell> cell in cells)
+            {
+                for (int y = cell.Key.y - 1; y <= (cell.Key.y + 1); y++)
+                {
+                    for (int x = cell.Key.x - 1; x <= (cell.Key.x + 1); x++)
+                    {
+                        int Neighbours = GetAliveNeighbours(x, y);
+                        Cell CurrentCell;
+                        bool Exists = cells.TryGetValue(new CellCoords(x, y), out CurrentCell);
+                        switch (Neighbours)
+                        {
+                            case 2:
+                                if (Exists)
+                                {
+                                    newCells.TryAdd(new CellCoords(x, y), CurrentCell);
+                                }
+                                break;
+                            case 3:
+                                if (Exists == false)
+                                {
+                                    bool Added = newCells.TryAdd(new CellCoords(x, y), CurrentCell);
+                                    if (Added)
+                                    {
+                                        //TODO: increase heat+
+                                    }
+                                }
+                                else
+                                {
+                                    newCells.TryAdd(new CellCoords(x, y), CurrentCell);
+                                }
+                                break;
+                            default:
+                                if (Exists)
+                                {
+                                    //TODO: decrease heat-
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+
+            cells = newCells;
+        }
+
         private int GetAliveNeighbours(int CoordX, int CoordY)
         {
             int Neighbours = 0;
-            for (int x = -1; x < 2; x++)
+            for (int x = -1; x <= 1; x++)
             {
-                for (int y = -1; y < 2; y++)
+                for (int y = -1; y <= 1; y++)
                 {
                     if (x != 0 || y != 0)
                     {
