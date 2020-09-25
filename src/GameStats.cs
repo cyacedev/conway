0db3f16046b3911b6c3359f03ce8114031a7d4ce
Ramon Segmüller <ramon.segmueller@sfs.biz>
2020-09-25 07:51:47 +0200

    ✨ added average neighbour for living cells

--- src/GameStats.cs
+++ src/GameStats.cs
@@ -8,9 +8,11 @@ namespace Conway
         public int PositiveHeat { get; set; }
         public int NegativeHeat { get; set; }
         public int CellCount { get; set; }
+        public float IterationAverageNeighbours { get; set; }
 
         public static void WriteStats(List<IterationStats> stats, string path)
         {
+
             using (var writer = new System.IO.StreamWriter(path))
             using (var csv = new CsvHelper.CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
             {
